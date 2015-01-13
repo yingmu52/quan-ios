fb8b940a3cda8a00e22bed488cfacf3f24f8b6ce
XInyi <yingmu52@gmail.com>
2015-01-13 14:02:59 -0800

    Initial Commit

---
+++ Wish/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Wish
+//
+//  Created by Xinyi Zhuang on 2015-01-13.
+//  Copyright (c) 2015 Xinyi Zhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Wish/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Wish
+//
+//  Created by Xinyi Zhuang on 2015-01-13.
+//  Copyright (c) 2015 Xinyi Zhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
