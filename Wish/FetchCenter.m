0b0c8c22746bb75b09f7a8a56a62f9efea104229
XInyi <yingmu52@gmail.com>
2015-01-26 12:47:19 -0800

    fucking upload image done !

--- Wish/FetchCenter.m
+++ Wish/FetchCenter.m
@@ -7,10 +7,8 @@
 //
 
 #import "FetchCenter.h"
-#import "AFHTTPRequestOperationManager.h"
-#import "AFURLSessionManager.h"
+@import Foundation;
 #define BASE_URL @"http://182.254.167.228/superplan/"
-
 #define PLAN @"plan/"
 #define GET_LIST @"splan_plan_getlist.php"
 #define PIC @"pic/"
@@ -20,91 +18,73 @@
 @implementation FetchCenter
 
 
-+ (AFHTTPRequestOperationManager *)launchManager{
-    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"text/html"]; //important !!!
-    return manager;
++ (NSURLRequest *)request:(NSString *)rqtStr method:(NSString *)method
+{
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:rqtStr] cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:30.0];
+    request.HTTPMethod = method;
+
+    return [request copy];
 }
 
 + (void)fetchPlanList:(NSString *)ownerId{
-    NSString *rqtStr = [NSString stringWithFormat:@"%@%@%@",BASE_URL,PLAN,GET_LIST];
-    NSDictionary *args = @{@"id":ownerId};
+    NSString *rqtStr = [NSString stringWithFormat:@"%@%@%@?id=%@",BASE_URL,PLAN,GET_LIST,ownerId];
 
+    NSURLSession *sesstion = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
     
-    AFHTTPRequestOperationManager *manager = [[self class] launchManager];
-    [manager GET:rqtStr
-      parameters:args
-         success:^(AFHTTPRequestOperation *operation, NSDictionary *responseObject) {
-             NSLog(@"JSON: %@", responseObject);
-    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-        NSLog(@"Error: %@", error);
+    NSURLRequest *request = [self.class request:rqtStr method:@"GET"];
+    NSURLSessionDataTask *task = [sesstion dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        //
+        NSLog(@"%@",[NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:nil]);
     }];
+    [task resume];
+
 }
 
 
 
 + (void)uploadToCreatePlan:(Plan *)plan{
     
-    NSString *rqtCreatePlan = [NSString stringWithFormat:@"%@%@%@",BASE_URL,PLAN,CREATE_PLAN];
     NSString *rqtUploadImage = [NSString stringWithFormat:@"%@%@%@",BASE_URL,PIC,UPLOAD_IMAGE];
 
     //upload image
-    AFHTTPRequestOperationManager *uploadMgr = [self.class launchManager];
+    NSData *imgData = UIImageJPEGRepresentation(plan.image, 0.5);
 
-    AFHTTPRequestOperation *upload = [uploadMgr POST:rqtUploadImage
-                                          parameters:nil
-                     constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
-                         [formData appendPartWithFileData:UIImageJPEGRepresentation(plan.image, 0.5)
-                                                     name:@"image"
-                                                 fileName:@"image.jpg"
-                                                 mimeType:@"image/jpeg"];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
 
-    } success:^(AFHTTPRequestOperation *operation, NSDictionary *imgResponse) {
-        //get image id
-        NSString *fetchedImageId = imgResponse[@"data.id"];
-        NSUInteger daysToFinish = [SystemUtil daysBetween:[NSDate date]
-                                                      and:plan.finishDate];
-        NSDictionary *createPlanArgs = @{@"ownerId":plan.ownerId,
-                                         @"title":plan.planTitle,
-                                         @"finishDate":@(daysToFinish),
-#warning background num selection in fetch center
-                                         @"backGroudNum":@1,
-                                         @"private":plan.isPrivate,
-#warning fetch tasks (Tasks obj)
-                                         @"subPlanList":@[]};
-        
-        
-        AFHTTPRequestOperationManager *createMgr = [[self class] launchManager];
-        //create plan and get plan id
-        [createMgr GET:rqtCreatePlan
-          parameters:createPlanArgs
-             success:^(AFHTTPRequestOperation *createPlan, NSDictionary *planResponse) {
-                 
-                 //get fetched plan id
-                 NSString *fetchedPlanId = planResponse[@"data.id"];
-                 
-                 //update core data - cloud and local synchronization
-                 plan.imageId = fetchedImageId;
-                 plan.planId = fetchedPlanId;
-                 [plan.managedObjectContext save:nil];
-                 
-                 NSLog(@"image id %@",fetchedImageId);
-                 NSLog(@"plan id %@",fetchedPlanId);
-                 
-             }failure:^(AFHTTPRequestOperation *createPlan, NSError *error) {
-                 NSLog(@"Fail to create plan: %@", error);
-             }];
-        
-
-    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-        NSLog(@"Error: %@ ***** %@", operation.responseString, error);
-    }];
-    [upload start];
+    NSURLRequest *request = [self.class request:rqtUploadImage method:@"POST"];
     
+    NSURLSessionUploadTask *uploadTask = [session uploadTaskWithRequest:request fromData:imgData completionHandler:^(NSData *data,NSURLResponse *response,NSError *error)
+    {
+        if (!error){ //upload image successed
+            
+            NSDictionary *imgJson = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:nil];
+            //get image id
+            plan.imageId = imgJson[@"data.id"];
+            
+            NSString *rqtCreatePlan = [NSString stringWithFormat:@"%@%@%@?ownerId=%@title=%@finishDate=%dbackGroudNum=%dprivate=%@",BASE_URL,PLAN,CREATE_PLAN,plan.ownerId,plan.planTitle,[SystemUtil daysBetween:[NSDate date] and:plan.finishDate],1,plan.isPrivate];
+            rqtCreatePlan = [rqtCreatePlan stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+            
+            NSURLRequest *request = [self.class request:rqtCreatePlan method:@"GET"];
+            NSURLSessionDataTask *creatTask = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                
+                if (!error) { //create plan success
+                    NSDictionary *planJson = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:nil];
+                    //get fetched plan id
+                    
+                    //update core data - cloud and local synchronization
+                    plan.planId = planJson[@"data.id"];
+                    if([plan.managedObjectContext save:nil]){
+                        NSLog(@"upload plan successed");
+                    }
+
+                }
+            }];
+            [creatTask resume];
+            
+        }
+                                                    
+    }];
+    [uploadTask resume];
     
 }
-
-
-
-
 @end
