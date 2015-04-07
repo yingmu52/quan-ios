67baf57ba86e97996e654713b3ed45e1451e99c9
XInyi <yingmu52@gmail.com>
2015-04-07 10:36:11 -0700

    modification on discover cell for production version 1.0

--- Wish/Discovery/DiscoveryCell.h
+++ Wish/Discovery/DiscoveryCell.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
-#define NORMALCELLID @"DiscoveryCell"
+//#define NORMALCELLID @"DiscoveryCell"
+#define NORMALCELLID @"DiscoveryCellTmp"
 
 @interface DiscoveryCell : UICollectionViewCell
 @property (weak, nonatomic) IBOutlet UILabel *discoveryFollowerCountLabel;
