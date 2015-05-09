bf4c98a6af765c13a38b24efb93126c7481e7e8b
XInyi <yingmu52@gmail.com>
2015-05-09 12:09:29 -0700

    update post detail UI

--- Wish/EditWishViewController.m
+++ Wish/EditWishViewController.m
@@ -20,124 +20,124 @@
 @end
 @implementation EditWishViewController
 
-- (void)viewDidLoad{
-    [super viewDidLoad];
-    [self setupTextField];
-    self.textField.text = self.plan.planTitle;
-    self.finishDateLabel.text = [NSString stringWithFormat:@"预计%@达成",[SystemUtil stringFromDate:self.plan.finishDate]];
-    self.isPrivate = self.plan.isPrivate.boolValue;
-    self.title = @"编辑愿望";
-}
-
-- (void)setupTextField{
-    self.textField.leftView = [[UIView alloc] initWithFrame:CGRectMake(0,0,10,1)];
-    self.textField.leftViewMode = UITextFieldViewModeAlways;
-    [self setViewBorder:self.textField];
-}
-
-- (void)doneEditing{
-    NSLog(@"test");
-    UIActivityIndicatorView *spinner = [[UIActivityIndicatorView alloc] initWithFrame:self.nextButton.frame];
-    spinner.activityIndicatorViewStyle = UIActivityIndicatorViewStyleGray;
-    [spinner startAnimating];
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:spinner];
-    self.backButton.enabled = NO;
-    
-    
-    [self.plan updatePlan:self.textField.text finishDate:self.selectedDate isPrivated:self.isPrivate];
-    
-    //set updaing request
-    FetchCenter *fc = [[FetchCenter alloc] init];
-    fc.delegate = self;
-    [fc updatePlan:self.plan];
-}
-
-
-- (IBAction)finishDateIsTapped:(UITapGestureRecognizer *)sender{
-    [super finishDateIsTapped:sender];
-    NSLog(@"test");
-    if (self.textField.isFirstResponder) [self.textField resignFirstResponder];
-}
-
-
-- (void)didFinishUpdatingPlan:(Plan *)plan{
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:self.nextButton];
-    self.backButton.enabled = YES;
-    [self.navigationController popToRootViewControllerAnimated:YES];
-    [((AppDelegate *)[[UIApplication sharedApplication] delegate]) saveContext];
-}
-- (void)didFailSendingRequestWithInfo:(NSDictionary *)info entity:(NSManagedObject *)managedObject{
-    //update navigation item
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:self.nextButton];
-    self.backButton.enabled = YES;
-    
-    //show alerts
-    [[[UIAlertView alloc] initWithTitle:[NSString stringWithFormat:@"%@",info[@"ret"]]
-                                message:[NSString stringWithFormat:@"%@",info[@"msg"]]
-                               delegate:self
-                      cancelButtonTitle:@"OK"
-                      otherButtonTitles:nil, nil] show];
-    
-    AppDelegate *delegate = [[UIApplication sharedApplication] delegate];
-    [delegate.managedObjectContext rollback];
-    [delegate.managedObjectContext refreshObject:self.plan mergeChanges:NO];
-}
-
-
-//- (void)dismissPickerView:(UIBarButtonItem *)item{
-//    [super dismissPickerView:item];
-////    if (!self.textField.isFirstResponder) [self.textField becomeFirstResponder];
+//- (void)viewDidLoad{
+//    [super viewDidLoad];
+//    [self setupTextField];
+//    self.textField.text = self.plan.planTitle;
+//    self.finishDateLabel.text = [NSString stringWithFormat:@"预计%@达成",[SystemUtil stringFromDate:self.plan.finishDate]];
+//    self.isPrivate = self.plan.isPrivate.boolValue;
+//    self.title = @"编辑愿望";
 //}
-
-
-
-- (IBAction)tapOnBackground:(UITapGestureRecognizer *)sender{
-    if (self.textField.isFirstResponder) [self.textField resignFirstResponder];
-}
-
-- (IBAction)giveUp:(UIButton *)sender{
-    [self showPopViewFor:PlanStatusGiveTheFuckingUp];
-}
-
-- (IBAction)finish:(UIButton *)sender{
-    [self showPopViewFor:PlanStatusFinished];
-}
-
-
-- (void)showPopViewFor:(PlanStatus)status{
-    UIView *keyWindow = [[UIApplication sharedApplication] keyWindow];
-    if (status == PlanStatusFinished) {
-        self.popView = [PopupView showPopupFinishinFrame:keyWindow.frame];
-    }else if (status == PlanStatusGiveTheFuckingUp){
-        self.popView = [PopupView showPopupFailinFrame:keyWindow.frame];
-    }
-    self.popView.delegate = self;
-    [keyWindow addSubview:self.popView];
-}
-
-
-#pragma mark - pop up view delegate
-
-
-- (void)popupViewDidPressCancel:(PopupView *)popupView{
-    [popupView removeFromSuperview];
-}
-
-- (void)popupViewDidPressConfirm:(PopupView *)popupView{
-    
-    if (popupView.state == PopupViewStateFinish){
-        [self.plan updatePlanStatus:PlanStatusFinished];
-
-    }else if (popupView.state == PopupViewStateGiveUp){
-        [self.plan updatePlanStatus:PlanStatusGiveTheFuckingUp];
-
-    }else {
-        NSAssert(false, @"error");
-    }
-    [popupView removeFromSuperview];
-    [self.navigationController popToRootViewControllerAnimated:YES];
-}
-
+//
+//- (void)setupTextField{
+//    self.textField.leftView = [[UIView alloc] initWithFrame:CGRectMake(0,0,10,1)];
+//    self.textField.leftViewMode = UITextFieldViewModeAlways;
+//    [self setViewBorder:self.textField];
+//}
+//
+//- (void)doneEditing{
+//    NSLog(@"test");
+//    UIActivityIndicatorView *spinner = [[UIActivityIndicatorView alloc] initWithFrame:self.nextButton.frame];
+//    spinner.activityIndicatorViewStyle = UIActivityIndicatorViewStyleGray;
+//    [spinner startAnimating];
+//    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:spinner];
+//    self.backButton.enabled = NO;
+//    
+//    
+//    [self.plan updatePlan:self.textField.text finishDate:self.selectedDate isPrivated:self.isPrivate];
+//    
+//    //set updaing request
+//    FetchCenter *fc = [[FetchCenter alloc] init];
+//    fc.delegate = self;
+//    [fc updatePlan:self.plan];
+//}
+//
+//
+//- (IBAction)finishDateIsTapped:(UITapGestureRecognizer *)sender{
+//    [super finishDateIsTapped:sender];
+//    NSLog(@"test");
+//    if (self.textField.isFirstResponder) [self.textField resignFirstResponder];
+//}
+//
+//
+//- (void)didFinishUpdatingPlan:(Plan *)plan{
+//    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:self.nextButton];
+//    self.backButton.enabled = YES;
+//    [self.navigationController popToRootViewControllerAnimated:YES];
+//    [((AppDelegate *)[[UIApplication sharedApplication] delegate]) saveContext];
+//}
+//- (void)didFailSendingRequestWithInfo:(NSDictionary *)info entity:(NSManagedObject *)managedObject{
+//    //update navigation item
+//    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:self.nextButton];
+//    self.backButton.enabled = YES;
+//    
+//    //show alerts
+//    [[[UIAlertView alloc] initWithTitle:[NSString stringWithFormat:@"%@",info[@"ret"]]
+//                                message:[NSString stringWithFormat:@"%@",info[@"msg"]]
+//                               delegate:self
+//                      cancelButtonTitle:@"OK"
+//                      otherButtonTitles:nil, nil] show];
+//    
+//    AppDelegate *delegate = [[UIApplication sharedApplication] delegate];
+//    [delegate.managedObjectContext rollback];
+//    [delegate.managedObjectContext refreshObject:self.plan mergeChanges:NO];
+//}
+//
+//
+////- (void)dismissPickerView:(UIBarButtonItem *)item{
+////    [super dismissPickerView:item];
+//////    if (!self.textField.isFirstResponder) [self.textField becomeFirstResponder];
+////}
+//
+//
+//
+//- (IBAction)tapOnBackground:(UITapGestureRecognizer *)sender{
+//    if (self.textField.isFirstResponder) [self.textField resignFirstResponder];
+//}
+//
+//- (IBAction)giveUp:(UIButton *)sender{
+//    [self showPopViewFor:PlanStatusGiveTheFuckingUp];
+//}
+//
+//- (IBAction)finish:(UIButton *)sender{
+//    [self showPopViewFor:PlanStatusFinished];
+//}
+//
+//
+//- (void)showPopViewFor:(PlanStatus)status{
+//    UIView *keyWindow = [[UIApplication sharedApplication] keyWindow];
+//    if (status == PlanStatusFinished) {
+//        self.popView = [PopupView showPopupFinishinFrame:keyWindow.frame];
+//    }else if (status == PlanStatusGiveTheFuckingUp){
+//        self.popView = [PopupView showPopupFailinFrame:keyWindow.frame];
+//    }
+//    self.popView.delegate = self;
+//    [keyWindow addSubview:self.popView];
+//}
+//
+//
+//#pragma mark - pop up view delegate
+//
+//
+//- (void)popupViewDidPressCancel:(PopupView *)popupView{
+//    [popupView removeFromSuperview];
+//}
+//
+//- (void)popupViewDidPressConfirm:(PopupView *)popupView{
+//    
+//    if (popupView.state == PopupViewStateFinish){
+//        [self.plan updatePlanStatus:PlanStatusFinished];
+//
+//    }else if (popupView.state == PopupViewStateGiveUp){
+//        [self.plan updatePlanStatus:PlanStatusGiveTheFuckingUp];
+//
+//    }else {
+//        NSAssert(false, @"error");
+//    }
+//    [popupView removeFromSuperview];
+//    [self.navigationController popToRootViewControllerAnimated:YES];
+//}
+//
 
 
 
--- Wish/PostDetailViewController.m
+++ Wish/PostDetailViewController.m
@@ -7,111 +7,41 @@
 //
 
 #import "PostDetailViewController.h"
-#import "SDWebImageCompat.h"
-@interface PostDetailViewController () 
+#import "Theme.h"
+#import "SystemUtil.h"
+@interface PostDetailViewController ()
+@property (weak, nonatomic) IBOutlet UIView *cameraBackground;
+
 @end
 @implementation PostDetailViewController
 
 
-@synthesize selectedDate = _selectedDate;
-
-
-- (NSDate *)tomorrow{
-    return [NSDate dateWithTimeIntervalSinceNow:3600]; // minimum date is from tomorrow on
-}
-
-- (NSDate *)selectedDate
-{
-    if (!_selectedDate) {
-        _selectedDate = [self tomorrow];
-    }
-    return _selectedDate;
-}
-- (void)setSelectedDate:(NSDate *)selectedDate
-{
-    _selectedDate = selectedDate;
-    NSString *dateString = [SystemUtil stringFromDate:selectedDate];
-    self.finishDateLabel.text = [NSString stringWithFormat:@"预计%@完成",dateString];
-
-}
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self setUpNavigationItem];
-    
-    self.finishDateLabel.text = [NSString stringWithFormat:@"预计%@完成",[SystemUtil stringFromDate:[NSDate date]]];
-    self.datePicker.backgroundColor = [UIColor whiteColor];
-    [self setViewBorder:self.dateBackground];
-    [self setViewBorder:self.isPrivateTab];
-
-}
-- (void)setViewBorder:(UIView *)view{
-    view.layer.borderColor = [Theme postTabBorderColor].CGColor;
-    view.layer.borderWidth = 1.0;
-}
-- (IBAction)finishDateIsTapped:(UITapGestureRecognizer *)sender {
-    self.bgView =[[UIView alloc] initWithFrame:self.view.bounds];
-    self.bgView.backgroundColor = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.6];
-    CGRect frame = CGRectMake(0, self.bgView.frame.size.height - 216.0, self.bgView.frame.size.width,216.0);
-    self.datePicker = [[UIDatePicker alloc] initWithFrame:frame];
-    self.datePicker.backgroundColor = [UIColor whiteColor];
-    self.datePicker.datePickerMode = UIDatePickerModeDate;
-    self.datePicker.minimumDate = [self tomorrow];
-
-    [self.datePicker addTarget:self action:@selector(pickerChanged:)
-              forControlEvents:UIControlEventValueChanged];
-
-    self.pickerToolBar = [[UIToolbar alloc] initWithFrame:CGRectMake(.0, self.datePicker.frame.origin.y - 44.0, self.datePicker.frame.size.width, 44.0)];
-    self.pickerToolBar.barTintColor = self.datePicker.backgroundColor;
-    self.pickerToolBar.translucent = NO;
-    UIBarButtonItem *cancel = [[UIBarButtonItem alloc] initWithTitle:@"取消"
-                                                               style:UIBarButtonItemStylePlain
-                                                              target:self
-                                                              action:@selector(dismissPickerView:)];
-    UIBarButtonItem *flexibleSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace
-                                                                                   target:self
-                                                                                   action:nil];
-    UIBarButtonItem *done = [[UIBarButtonItem alloc] initWithTitle:@"完成"
-                                                             style:UIBarButtonItemStylePlain
-                                                            target:self
-                                                            action:@selector(dismissPickerView:)];
-    
-    self.pickerToolBar.items = @[flexibleSpace,cancel,flexibleSpace,flexibleSpace,flexibleSpace,flexibleSpace,flexibleSpace,flexibleSpace,flexibleSpace,flexibleSpace,flexibleSpace,flexibleSpace,done,flexibleSpace];
-    
-    [self.bgView addSubview:self.datePicker];
-    [self.bgView addSubview:self.pickerToolBar];
-    [[[UIApplication sharedApplication] keyWindow] addSubview:self.bgView];
-
+    [self borderiseCameraBackground];
 }
 
-- (void)pickerChanged:(UIDatePicker *)picker{
-    self.selectedDate = picker.date;
+- (void)borderiseCameraBackground{
+    self.cameraBackground.layer.borderColor = [SystemUtil colorFromHexString:@"#33C6B4"].CGColor;
+    self.cameraBackground.layer.borderWidth = 1.5f;
 }
 
-- (void)dismissPickerView:(UIBarButtonItem *)item
-{
-    if ([item.title isEqualToString:@"取消"]) {
-        NSString *dateString = [SystemUtil stringFromDate:[NSDate date]];
-        self.finishDateLabel.text = [NSString stringWithFormat:@"预计%@完成",dateString];
-    }
-    [self.bgView removeFromSuperview];
-    [self.pickerToolBar removeFromSuperview];
-    [self.datePicker removeFromSuperview];
-}
 - (void)setUpNavigationItem
 {
     CGRect frame = CGRectMake(0,0, 25,25);
-    self.backButton = [Theme buttonWithImage:[Theme navBackButtonDefault]
+    UIButton *backButton = [Theme buttonWithImage:[Theme navBackButtonDefault]
                                         target:self.navigationController
                                       selector:@selector(popViewControllerAnimated:)
                                          frame:frame];
     
-    self.nextButton = [Theme buttonWithImage:[Theme navTikButtonDefault]
-                                           target:self
-                                         selector:@selector(doneEditing)
-                                            frame:frame];
+//    self.nextButton = [Theme buttonWithImage:[Theme navTikButtonDefault]
+//                                           target:self
+//                                         selector:@selector(doneEditing)
+//                                            frame:frame];
     
-    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:self.backButton];
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:self.nextButton];
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:backButton];
+//    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:self.nextButton];
     
     //set navigation bar title and color
     self.navigationController.navigationBar.titleTextAttributes = @{NSForegroundColorAttributeName:[SystemUtil colorFromHexString:@"#2A2A2A"]};
@@ -119,71 +49,4 @@
     
 }
 
-- (void)setIsPrivate:(BOOL)isPrivate
-{
-    _isPrivate = isPrivate;
-    if (isPrivate){
-        self.lockImageView.image = [Theme privacyIconSelected];
-        self.isPrivateLabel.text = @"私密愿望";
-        self.isPrivateTab.backgroundColor = [Theme privacyBackgroundSelected];
-    }else{
-        self.lockImageView.image = [Theme privacyIconDefault];
-        self.isPrivateLabel.text = @"设为私密愿望";
-        self.isPrivateTab.backgroundColor = [Theme privacyBackgroundDefault];
-    }
-    
-}
-- (IBAction)tapOnPrivacyTab:(UITapGestureRecognizer *)sender {
-    self.isPrivate = !self.isPrivate;
-}
-
-- (void)doneEditing
-{
-    UIActivityIndicatorView *spinner = [[UIActivityIndicatorView alloc] initWithFrame:self.nextButton.frame];
-    spinner.activityIndicatorViewStyle = UIActivityIndicatorViewStyleGray;
-    [spinner startAnimating];
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:spinner];
-
-    self.plan = [Plan createPlan:self.titleFromPostView
-                             date:self.selectedDate
-                          privacy:self.isPrivate
-                            image:nil];
-    FetchCenter *fc = [[FetchCenter alloc] init];
-    fc.delegate = self;
-    [fc uploadToCreatePlan:self.plan];
-//    self.backButton.enabled = NO;
-   
-}
-
-- (void)didFinishUploadingPlan:(Plan *)plan{
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:self.nextButton];
-    //        self.backButton.enabled = YES;
-    [self performSegueWithIdentifier:@"doneWirtingAPost" sender:plan];
-}
-
-- (void)didFailSendingRequestWithInfo:(NSDictionary *)info entity:(NSManagedObject *)managedObject{
-    //show alerts
-    [[[UIAlertView alloc] initWithTitle:[NSString stringWithFormat:@"%@",info[@"ret"]]
-                                message:[NSString stringWithFormat:@"%@",info[@"msg"]]
-                               delegate:self
-                      cancelButtonTitle:@"OK"
-                      otherButtonTitles:nil, nil] show];
-    
-    //remove plan from core data
-    if ([managedObject isKindOfClass:[Plan class]]){
-        Plan *plan = (Plan *)managedObject;
-        [plan deleteSelf];
-    }
-    
-    //update navigation item
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:self.nextButton];
-    //        self.backButton.enabled = YES;
-}
-
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(Plan *)sender
-{
-    if ([segue.identifier isEqualToString:@"doneWirtingAPost"]) {
-        [segue.destinationViewController setPlan:sender];
-    }
-}
 @end
