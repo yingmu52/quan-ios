98faad8d54aefc49ffa62748826a2a9e84177039
XInyi <yingmu52@gmail.com>
2015-05-28 23:12:10 -0700

    saved

--- Wish/MenuViewController.m
+++ Wish/MenuViewController.m
@@ -40,7 +40,7 @@ typedef enum {
 
 - (void)setVersionLabel:(UILabel *)versionLabel{
     _versionLabel = versionLabel;
-    _versionLabel.text = @"Version 3.0.2";
+    _versionLabel.text = @"Version 3.1.0";
 }
 - (IBAction)showSettingsView:(UIButton *)sender{
     [self performSegueWithIdentifier:@"showSettingView" sender:nil];
