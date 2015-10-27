05af06fa77d75900bb38fbf8135aec55cd593e75
XInyi <yingmu52@gmail.com>
2015-10-27 14:57:07 -0700

    添加微信分享~

--- Wish/Discovery/InvitationViewController.m
+++ Wish/Discovery/InvitationViewController.m
@@ -7,11 +7,31 @@
 //
 
 #import "InvitationViewController.h"
-
+#import "AppDelegate.h"
 @implementation InvitationViewController
 
 - (IBAction)dismissViewcontroller{
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)tapOnWechat{
+    WXMediaMessage *message = [WXMediaMessage message];
+    message.title = @"H5大师将至";
+    message.description = @"尽请期待！";
+    [message setThumbImage:nil];
+    WXWebpageObject *ext = [WXWebpageObject object];
+    ext.webpageUrl = @"www.pgyer.com/sAG9";
+    message.mediaObject = ext;
+    
+    SendMessageToWXReq *req = [[SendMessageToWXReq alloc] init];
+    req.bText = NO;
+    req.message = message;
+    req.scene = WXSceneSession;
+    [WXApi sendReq:req];
+}
+
+- (IBAction)tapOnQQ{
+    
 }
 
 @end
