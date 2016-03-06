0dc3e0c00729675e1a759387b6fc89119473ad17
XInyi <yingmu52@gmail.com>
2016-03-05 16:35:50 -0800

    Set up Empty Circle View

--- Wish/ButtonWithPathedImage.m
+++ Wish/ButtonWithPathedImage.m
@@ -22,14 +22,8 @@
     //拉伸背影图，注意这个做法需要背影图保持自己的真实高度
     UIImage *backgroundImage = [self backgroundImageForState:UIControlStateNormal];
     if (backgroundImage) {
-        
-        CGFloat top = CGRectGetHeight(self.frame) * 0.5;
-        CGFloat buttom = CGRectGetHeight(self.frame) * 0.5;
-        
-        CGFloat left = CGRectGetWidth(self.frame) * 0.5;
-        CGFloat right = CGRectGetWidth(self.frame) * 0.5;
-
-        backgroundImage = [backgroundImage resizableImageWithCapInsets:UIEdgeInsetsMake(top,left,buttom,right)];
+        CGFloat horizontal = backgroundImage.size.width * 0.5;
+        backgroundImage = [backgroundImage resizableImageWithCapInsets:UIEdgeInsetsMake(0,horizontal,0,horizontal)];
         [self setBackgroundImage:backgroundImage forState:UIControlStateNormal];
     }
 }
