09208fbc909f86ee1d2dc2d7fcedd11db0b048bb
XInyi <yingmu52@gmail.com>
2016-08-08 13:29:46 +0800

    有图片id时才下载图片

--- Wish/UIImageView+ImageCache.m
+++ Wish/UIImageView+ImageCache.m
@@ -13,47 +13,49 @@
 
 - (void)downloadImageWithImageId:(NSString *)imageId size:(FetchCenterImageSize)size{
     
-    SDWebImageManager *manager = [SDWebImageManager sharedManager];
-    FetchCenter *fetchCenter = [[FetchCenter alloc] init];
-    
-    //检查本地是否有更高分辨率的图片存在
-    NSURL *originalURL = [fetchCenter urlWithImageID:imageId size:FetchCenterImageSizeOriginal];
-    
-    if ([manager diskImageExistsForURL:originalURL]) {
-        self.image = [manager.imageCache imageFromDiskCacheForKey:[manager cacheKeyForURL:originalURL]];
-    }else{
-        //图片地址拼接
-        NSURL *url = [fetchCenter urlWithImageID:imageId size:size];
+    if (imageId) {
+        SDWebImageManager *manager = [SDWebImageManager sharedManager];
+        FetchCenter *fetchCenter = [[FetchCenter alloc] init];
         
-        //检测本地缓存
-        NSString *localKey = [manager cacheKeyForURL:url];
-        BOOL isTargetImageExist = [manager diskImageExistsForURL:url];
+        //检查本地是否有更高分辨率的图片存在
+        NSURL *originalURL = [fetchCenter urlWithImageID:imageId size:FetchCenterImageSizeOriginal];
         
-        //下载图片
-        if (!isTargetImageExist) {
-            [self sd_setImageWithURL:url
-                    placeholderImage:nil
-                             options:SDWebImageAvoidAutoSetImage
-                           completed:^(UIImage *image,
-                                       NSError *error,
-                                       SDImageCacheType cacheType,
-                                       NSURL *imageURL) {
-                               if (image && !error) {
-                                   dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-                                       [manager.imageCache storeImage:image forKey:localKey];
-                                       dispatch_main_async_safe(^{
-                                           //无图时才设置图，注意要配合SDWebImageAvoidAutoSetImage使用
-                                           if (!self.image) {
-                                               self.image = image;
-                                           }
-                                           
-                                       });
-                                   });
-                               }
-                           }];
+        if ([manager diskImageExistsForURL:originalURL]) {
+            self.image = [manager.imageCache imageFromDiskCacheForKey:[manager cacheKeyForURL:originalURL]];
         }else{
-            //        NSLog(@"可从本地获取");
-            self.image = [manager.imageCache imageFromDiskCacheForKey:localKey];
+            //图片地址拼接
+            NSURL *url = [fetchCenter urlWithImageID:imageId size:size];
+            
+            //检测本地缓存
+            NSString *localKey = [manager cacheKeyForURL:url];
+            BOOL isTargetImageExist = [manager diskImageExistsForURL:url];
+            
+            //下载图片
+            if (!isTargetImageExist) {
+                [self sd_setImageWithURL:url
+                        placeholderImage:nil
+                                 options:SDWebImageAvoidAutoSetImage
+                               completed:^(UIImage *image,
+                                           NSError *error,
+                                           SDImageCacheType cacheType,
+                                           NSURL *imageURL) {
+                                   if (image && !error) {
+                                       dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+                                           [manager.imageCache storeImage:image forKey:localKey];
+                                           dispatch_main_async_safe(^{
+                                               //无图时才设置图，注意要配合SDWebImageAvoidAutoSetImage使用
+                                               if (!self.image) {
+                                                   self.image = image;
+                                               }
+                                               
+                                           });
+                                       });
+                                   }
+                               }];
+            }else{
+                //        NSLog(@"可从本地获取");
+                self.image = [manager.imageCache imageFromDiskCacheForKey:localKey];
+            }
         }
     }
 }
